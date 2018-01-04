d0555a064eba3eeb2d7ee2259ba199d64d5a576c
caotinging <caotinging@outlook.com>
2018-01-04 08:39:43 +0800

    2018-1-4 通过实现方法进行授权

--- mavenBos/bos-service/src/main/java/caotinging/realm/MyRealm.java
+++ mavenBos/bos-service/src/main/java/caotinging/realm/MyRealm.java
@@ -1,25 +1,30 @@
 package caotinging.realm;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import caotinging.dao.IUserDao;
 import caotinging.domain.User;
 
-public class MyRealm extends AuthenticatingRealm {
+public class MyRealm extends AuthorizingRealm {
 
 	@Autowired
 	private IUserDao userDao;
 	
-	@Override
+	
 	/**
-	 * 用户登录逻辑
+	 * 用户登录逻辑---认证方法
 	 */
+	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationtoken)
 			throws AuthenticationException {
 		//首先将认证令牌转为用户名密码令牌
@@ -35,6 +40,25 @@ public class MyRealm extends AuthenticatingRealm {
 		
 		//用户名存在，将用户信息交给shiro框架进行密码比对
 		AuthenticationInfo info = new SimpleAuthenticationInfo(user, user.getPassword(), this.getName());
+		return info;
+	}
+
+
+	/**
+	 * 授权方法：给当前用户授权staff-list权限
+	 * @param principalcollection
+	 * @return
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalcollection) {
+		//获取授权信息
+		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+		//为用户增加staff-list权限
+		info.addStringPermission("staff-list");
+		
+		User user1 = (User) SecurityUtils.getSubject().getPrincipal();
+		User user2 = (User) principalcollection.getPrimaryPrincipal();
+		System.out.println(user1 == user2);
 		return info;
 	}
 
