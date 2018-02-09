2f27a33a6e051bb8829a66837f40e7bc217c208a
caotinging <caotinging@outlook.com>
2018-02-09 19:41:56 +0800

    customer信息查询

--- e3-ssm/src/caotinging/tools/QueryVo.java
+++ e3-ssm/src/caotinging/tools/QueryVo.java
@@ -4,9 +4,68 @@ import java.util.List;
 
 import caotinging.pojo.Items;
 
+/**
+ * 封装查询条件
+ * @author caoting
+ *
+ */
 public class QueryVo {
 
+	private String custName;
+	private String custSource;
+	private String custIndustry;
+	private String custLevel;
 	
+	private Integer currentPage;
+	private Integer pageSize;
+	
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	public String getCustSource() {
+		return custSource;
+	}
+	public void setCustSource(String custSource) {
+		this.custSource = custSource;
+	}
+	public String getCustIndustry() {
+		return custIndustry;
+	}
+	public void setCustIndustry(String custIndustry) {
+		this.custIndustry = custIndustry;
+	}
+	public String getCustLevel() {
+		return custLevel;
+	}
+	public void setCustLevel(String custLevel) {
+		this.custLevel = custLevel;
+	}
+	public Integer getCurrentPage() {
+		return currentPage;
+	}
+	public void setCurrentPage(Integer currentPage) {
+		this.currentPage = currentPage;
+	}
+	public Integer getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+	
+	/*
+	 * 计算起始条数
+	 */
+	public Integer getStartRows() {
+		Integer startRows = 0;
+		if(pageSize != null && currentPage != null) {
+			startRows = (currentPage - 1)*pageSize;
+		}
+		return startRows;
+	}
 	
 	private Items item;
 	
